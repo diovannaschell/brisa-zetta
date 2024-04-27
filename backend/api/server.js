5aadc5889a28666071f6c5781756231ed95ea4de
yperdoom <yperdoom@gmail.com>
2024-04-27 10:24:59 -0300

    add server file

---
+++ backend/api/server.js
@@ -0,0 +1,12 @@
+'use strict'
+
+require('dotenv/config')
+const { PORT } = process.env
+
+const api = require('./config/loadingRoutes')
+
+const Logger = require('./lib/Logger')
+
+api.listen(PORT, () => {
+    Logger.trace(`Api rodando na porta: ${PORT}`)
+})
