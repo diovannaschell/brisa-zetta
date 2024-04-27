7cefdde24623e8f5cf90d6f66e16f20abdb83364
yperdoom <yperdoom@gmail.com>
2024-04-27 10:23:21 -0300

    feat: add api configurations

---
+++ backend/api/config/apiConfiguration.js
@@ -0,0 +1,21 @@
+'use strict'
+
+const bodyParser = require('body-parser')
+const express = require('express')
+const cors = require('cors')
+const api = express()
+
+api.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Origin', 'GET, PUT, POST, DELETE')
+    api.use(cors())
+    next()
+})
+
+api.use(
+    bodyParser.urlencoded({
+        extended: true
+    })
+)
+
+module.exports = api
