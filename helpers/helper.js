f7ed68328c9465781bc0bffbae2cbea9a0dd5e78
jeffleyg <jeffleygarcon007@gmail.com>
2024-05-17 15:35:43 -0300

    Added new config, controllers, helpers, and services; removed app.js

---
+++ helpers/helper.js
@@ -0,0 +1,34 @@
+const fetch = require('node-fetch');
+const stringSimilarity = require('string-similarity');
+const { usuariosCollection } = require('../config/mongo');
+
+async function getCoordinates(address) {
+  const url = `https://nominatim.openstreetmap.org/search?format=json&limit=1&q=${encodeURIComponent(address)}`;
+  const response = await fetch(url);
+  const data = await response.json();
+  return data.length > 0 ? data[0] : null;
+}
+
+async function findFrequentBairro(municipio, uf) {
+  const frequentBairro = await usuariosCollection.aggregate([
+    { $match: { municipio, uf } },
+    { $group: { _id: "$bairro", count: { $sum: 1 } } },
+    { $sort: { count: -1 } },
+    { $limit: 1 }
+  ]).toArray();
+  return frequentBairro.length > 0 ? frequentBairro[0]._id : null;
+}
+
+async function findSimilarBairro(bairro, municipio, uf) {
+  const bairros = await usuariosCollection.distinct("bairro", { municipio, uf });
+  const matches = stringSimilarity.findBestMatch(bairro, bairros);
+  return matches.bestMatch.target;
+}
+
+async function findSimilarStreet(street, municipio, uf) {
+  const streets = await usuariosCollection.distinct("endereco", { municipio, uf });
+  const matches = stringSimilarity.findBestMatch(street, streets);
+  return matches.bestMatch.target;
+}
+
+module.exports = { getCoordinates, findFrequentBairro, findSimilarBairro, findSimilarStreet };
